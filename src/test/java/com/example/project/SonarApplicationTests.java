2385531635f39a738efedb15fc60dd6288bad13f
Sahrul1610 <sahrulfyou@gmail.com>
2024-09-12 17:26:31 +0700

    tes1

--- src/test/java/com/example/project/SonarApplicationTests.java
+++ src/test/java/com/example/project/SonarApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.project;
 
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SonarApplicationTests {
+@SpringBootTest(classes = SonarApplication.class)
+class GarageServiceTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private GarageService garageService;
+
+    @Test
+    void rentAGarage() {
+        Person person = Person.builder()
+                .name("Rico")
+                .gender('L')
+                .build();
+
+        List<Car> cars = List.of(
+                Car.builder().name("Lambo").color("Black").build(),
+                Car.builder().name("Ferrari").color("Red").build(),
+                Car.builder().name("Mustang GT").color("Gray").build());
+
+        Garage rentedGarage = garageService.rentGarage(person, cars);
+
+        Assertions.assertSame(person.getName(), rentedGarage.getOwner().getName());
+        Assertions.assertSame(person.getGender(), rentedGarage.getOwner().getGender());
+        Assertions.assertSame(cars.get(0).getName(), rentedGarage.getCars().get(0).getName());
+        Assertions.assertSame(cars.get(0).getColor(), rentedGarage.getCars().get(0).getColor());
+        Assertions.assertSame(cars.get(1).getName(), rentedGarage.getCars().get(1).getName());
+        Assertions.assertSame(cars.get(2).getName(), rentedGarage.getCars().get(2).getName());
+    }
 
 }
+
